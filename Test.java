c27543c501028a62c5a41027746030d0c007f086
Jeyakanthan J <jjeyakanthan2005@gmail.com>
2026-05-25 16:40:44 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Test{
+
+public static void main(String[] args) {
+   //System.out.println(i++ + ++i);
+
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int a = in.nextInt();
+}
+}
---
+++ test2.java
@@ -0,0 +1,7 @@
+class test2 {
+
+public static void main(String[] args) {
+int i = 2;
+System.out.println(i++ + ++i);
+}
+}
