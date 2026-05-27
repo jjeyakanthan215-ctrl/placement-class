11b9c64fde9a5eb9b6c0bfab114df1f60f01f649
Jeyakanthan J <jjeyakanthan2005@gmail.com>
2026-05-27 14:06:47 +0530

    day3

---
+++ day2/fibonac.java
@@ -0,0 +1,13 @@
+public class fibonac {
+    public static void main(String[] args) {
+        int n = 10; // Number of terms to print
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
